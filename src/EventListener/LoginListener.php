ac270140fc61c7a1a4604408365aa284c9d0c431
Fabian Wolf <fabianwolf@Fabians-MacBook-Pro-2.local>
2023-12-27 22:30:31 +0100

    WIP: logout listener

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -10,16 +10,20 @@ use App\Entity\ShoppingCartProduct;
 use Doctrine\Common\Lexer\Token;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use App\Services\ProductService;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\Security\Core\Security;
 
 class LoginListener
 {    private $requestStack;
     private $entityManager;
     private $productService;
-    public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager, ProductService $productService)
+    private $security;
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager, ProductService $productService, Security $security)
     {
         $this->requestStack = $requestStack;
         $this->entityManager = $entityManager;
         $this->productService = $productService;
+        $this->security = $security;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
@@ -49,5 +53,25 @@ class LoginListener
                 $this->productService->mergeShoppingCarts($oldShoppingCart, $newShoppingCart);
             }
         }
+    }
+    public function onSecurityLogout(LogoutEvent $event)
+    {
+
+        $user = $this->security->getUser();
+
+        if ($user) {
+
+            //TODO
+
+        }
+        // $session = $this->requestStack->getSession();
+        // $sessionId = $session->getId();
+        // $shoppingCart = $this->entityManager->getRepository(ShoppingCart::class)->findOneBy(['sessionId' => $sessionId]);
+        // $shoppingCartProducts = $this->entityManager->getRepository(ShoppingCartProduct::class)->findBy(['shoppingCart' => $shoppingCart->getId()]);
+        // foreach ($shoppingCartProducts as $shoppingCartProduct) {
+        //     $this->entityManager->remove($shoppingCartProduct);
+        // }
+        // $this->entityManager->remove($shoppingCart);
+        // $this->entityManager->flush();
     }
 }
